6053e8c5b1d473c68bc85552cfc10b316bcd03d5
U-DESKTOP-29S45JJ\Thomas <tjhacker@purdue.edu>
2021-10-27 11:23:02 -0400

    Version for bare metal

---
+++ make_spoke_bare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p mspoke/certbundle/centos8
+cp spoke/Vagrantfile.bare mspoke/Vagrantfile
+cp -r spoke/* mspoke/certbundle
+cp -r common/* mspoke/certbundle
+#??rm mspoke/certbundle/Vagrantfile
+cp pam_duo.conf mspoke/certbundle
+cd mspoke
+vagrant up
+vagrant ssh 
